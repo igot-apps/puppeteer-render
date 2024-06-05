185a999ce6c82debec9ee44b9f917ca1650d6e0b
igot-apps <aagortey@gmail.com>
2024-06-05 23:15:34 +0000

    example app

---
+++ scrapeLogic.js
@@ -0,0 +1,46 @@
+const puppeteer = require("puppeteer");
+require("dotenv").config();
+
+const scrapeLogic = async (res) => {
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+        headless: true
+  });
+  try {
+    const page = await browser.newPage();
+
+    await page.goto("https://developer.chrome.com/");
+
+    // Set screen size
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    const selector = "h2#a-powerful-web-span-stylecolor-var-chrome-primarymade-easierspan";
+
+    const element = await page.$(selector);
+  
+    
+    
+    const value = await page.evaluate((el) => {
+       return el.textContent;
+    }, element);
+    
+    console.log("the exacted email is" , value); // Print the extracted value
+    res.send(value);
+  } catch (e) {
+    console.error(e);
+    res.send(`Something went wrong while running Puppeteer: ${e}`);
+  } finally {
+    await browser.close();
+  }
+};
+
+module.exports = { scrapeLogic };
